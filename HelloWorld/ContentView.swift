c3c1c1663f3fad6c03ea4cdd940b43741b30606d
Angelo De Laurentis <angeldelaur@gmail.com>
2019-11-17 14:40:31 -0800

    Adding Demo Button

--- HelloWorld/ContentView.swift
+++ HelloWorld/ContentView.swift
@@ -10,7 +10,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World how are you")
+//        Text("Hello World how are you");
+        Button(action: {
+            // your action here
+            print("button clicked");
+        }) {
+            Text("Button title");
+        }
+
     }
 }
 
