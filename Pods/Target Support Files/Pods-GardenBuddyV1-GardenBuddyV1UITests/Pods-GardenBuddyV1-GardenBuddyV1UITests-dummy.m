7b04ded12724946e97d9c8d51b751fe7225d38f7
Angelo De Laurentis <angeldelaur@gmail.com>
2019-11-17 15:28:49 -0800

    build changes for aws

---
+++ Pods/Target Support Files/Pods-GardenBuddyV1-GardenBuddyV1UITests/Pods-GardenBuddyV1-GardenBuddyV1UITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GardenBuddyV1_GardenBuddyV1UITests : NSObject
+@end
+@implementation PodsDummy_Pods_GardenBuddyV1_GardenBuddyV1UITests
+@end
---
+++ Pods/Target Support Files/Pods-GardenBuddyV1Tests/Pods-GardenBuddyV1Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GardenBuddyV1Tests : NSObject
+@end
+@implementation PodsDummy_Pods_GardenBuddyV1Tests
+@end
