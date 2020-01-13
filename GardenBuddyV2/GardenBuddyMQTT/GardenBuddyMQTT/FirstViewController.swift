62e61fe3e134d8d4114b5baa5dfaeb0e32e1a44a
Nick Balestrino <nbalestrino98@gmail.com>
2020-01-13 15:23:38 -0800

    MQTT

---
+++ GardenBuddyV2/GardenBuddyMQTT/GardenBuddyMQTT/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  GardenBuddyMQTT
+//
+//  Created by Nicholas Balestrino on 1/13/20.
+//  Copyright © 2020 Misc. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GardenBuddyV2/GardenBuddyMQTT/GardenBuddyMQTT/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  GardenBuddyMQTT
+//
+//  Created by Nicholas Balestrino on 1/13/20.
+//  Copyright © 2020 Misc. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
