bd0f50cc969f9b775fc26ce60227269e35dfce73
Charolf <36802475+Charolf@users.noreply.github.com>
2020-03-04 21:18:07 -0800

    newest proj code

--- YardWorkModeMaster/YardWorkMode_3/PopUpView.swift
+++ YardWorkModeMaster/YardWorkMode_3/PopUpView.swift
@@ -8,39 +8,44 @@
 
 import UIKit
 import SwiftSocket
+import AWSIoT
 
 class PopUpView: UIViewController {
-      
+    
     let host = "192.168.43.78"    // 192.168.43.78 192.168.43.121
     let port = 7                  // 7
     var client: TCPClient?
     
+//    let iotDataManager = AWSIoTDataManager(forKey: ASWIoTDataManager)
+//    let tabBarViewController = tabBarController as! IoTSampleTabBarController
+    
     @IBOutlet weak var popupArea: UIView!
     
-    @IBOutlet weak var ZoneInput: UILabel?
+    @IBOutlet weak var connectOrNot: UILabel! //mqtt or tcp
+    
+    @IBOutlet weak var ZoneInput: UINavigationItem! //zone #
     
     @IBOutlet weak var CountdownTimer: UIDatePicker!
+    
     var timerValue: Double = 0
     var prevValue: Double = 0
     
-    struct ZoneData {
-        var currentStatus: String
-        var zoneTitle: [String] = []
-        var zoneToggleButton: UIButton
-        var zoneToggleText: String
-    }
-    //var zoneData:
+    var zoneToggleText: String = ""
+    var connectionStatus: String = ""
+    
+    var zoneName = "Idk zone"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //ZoneInput?.text = zoneData?.zoneTitle
+        ZoneInput.title! = zoneName
+        connectOrNot.text = connectionStatus
         
         CountdownTimer.addTarget(self, action: #selector(PopUpView.countdownChanged(CountdownTimer:)), for: UIControl.Event.valueChanged)
     
-        self.navigationItem.hidesBackButton = true
+        //self.navigationItem.hidesBackButton = true
         popupArea.layer.cornerRadius = 15
-        popupArea.layer.masksToBounds = true
+        //popupArea.layer.masksToBounds = true
         
         // TCPClient
         client = TCPClient(address: host, port: Int32(port))
@@ -52,31 +57,85 @@ class PopUpView: UIViewController {
         //let strCount = countdownFormat.string(from: NSNumber(value: CountdownTimer.countDownDuration))
         //ZoneInput?.text = strCount
     }
-    
-    //need to change status when confirm
-    @IBAction func cancelPopUp(_ sender: Any) {
+    @IBAction func turnOff(_ sender: Any) {
+        timerValue = 0.0
+        let zone_n = caseZone()
+        
+        if (connectionStatus == "Connected") {  // MQTT
+            print("It's MQTT")
+        }
+        else {                                  // TCP
+            TCPconnection(zone_number: zone_n)
+        }
+        //current status: off
+        self.navigationController?.popViewController(animated: true)
         dismiss(animated: true, completion: nil)
     }
     
-    //
-    @IBAction func confirmPopUp(sender: UIButton) {
-        guard let client = client else { return }
+    @IBAction func confirmPopUp(_ sender: Any) {
         //duration clicked
         timerValue = CountdownTimer.countDownDuration
-        let ywz = YardWorkZone(zone_num: 1, duration: Int(timerValue)).convertToJSONString()
-        print(ywz)
-        switch client.connect(timeout: 10) {
-        case .success:
-            print("Connected to host \(client.address)")
-            if let response = sendRequest(string: ywz, using: client) {   // "YW_"+ywz
-                print( "Response: \(response)")
-            }
-        case .failure(let error):
-            print(String(describing: error))
+        let zone_n = caseZone()
+        
+        if (connectionStatus == "Connected") {  // MQTT
+            print("It's MQTT")
+        }
+        else {                                  // TCP
+            TCPconnection(zone_number: zone_n)
         }
         //get zone title
         //change status to "Current Status: On"
+        self.navigationController?.popViewController(animated: true)
         dismiss(animated: true, completion: nil)
+    }
+    
+    private func TCPconnection(zone_number: Int) {
+        guard let client = client else { return }
+        //duration clicked
+        let ywz = "YW_"+YardWorkZone(zone_num: zone_number, duration: Int(timerValue)).convertToJSONString()
+        print(ywz)
+        switch client.connect(timeout: 5) {
+        case .success:
+            print("Connected to host \(client.address)")
+        case .failure(let error):
+            print(String(describing: error))
+            return
+        }
+        if let response = sendRequest(string: ywz, using: client) {
+            print( "Response: \(response)")
+        }
+    }
+    
+    private func caseZone() -> Int {
+        let zone = String(ZoneInput.title!)
+        switch (zone) {
+        case "Zone 1":
+            return 1
+        case "Zone 2":
+            return 2
+        case "Zone 3":
+            return 3
+        case "Zone 4":
+            return 4
+        case "Zone 5":
+            return 5
+        case "Zone 6":
+            return 6
+        case "Zone 7":
+            return 7
+        case "Zone 8":
+            return 8
+        case "Zone 9":
+            return 9
+        case "Zone 10":
+            return 10
+        case "Zone 11":
+            return 11
+        case "Zone 12":
+            return 12
+        default:
+            return 0
+        }
     }
     
     private func readResponse(from client: TCPClient, string: String) -> String? {
@@ -88,7 +147,11 @@ class PopUpView: UIViewController {
         }
         total += ((string.count) / len)
         while counter < total {
-            guard let response = client.read(len, timeout: 10) else { print("readResponse is nil")
+            guard let response = client.read(len, timeout: 5) else {
+//                let alert = UIAlertController(title: "Alert", message: "readResponse is nil", preferredStyle: .alert)
+//                alert.addAction(UIAlertAction(title: "Click", style: UIAlertAction.Style.default, handler: nil))
+//                self.present(alert, animated: true, completion: nil)
+                print("readResponse is nil")
                 return nil
             }
             counter += 1
@@ -111,26 +174,6 @@ class PopUpView: UIViewController {
       }
     }
     
-    //cancel and confirm functions
-    //@IBAction func CancelOp(_ sender: UIButton) {
-        //self.removeAnimate()
-    //}
-    //override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        //let destVC = segue.destination as! ViewController
-
-        //if segue.identifier == "unwindToMainConfirm" {
-          //  print("confirmed")
-            //destVC.currentstatus = "Current Status: On"
-        //}
-        //else if segue.identifier == "unwindToMainCancel" {
-            //print("cancelled")
-            //destVC.currentstatus = "Current Status: Off"
-        //}
-        //else {
-          //  print("errorrrrr")
-       // }
-    //}
-   
     /*
     // MARK: - Navigation
 
